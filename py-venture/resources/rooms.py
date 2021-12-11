a37441643ef7d89f1c3dd0b05107a4399af740e8
DVDTSB <66365801+DVDTSB@users.noreply.github.com>
2021-12-11 23:27:39 +0200

    a

--- py-venture/resources/rooms.py
+++ py-venture/resources/rooms.py
@@ -1,1 +1,18 @@
+from modules.room import Room
 from premade.objects.rooms import *
+import items as items
+
+starting_room = room.Room(
+    "Starting room",
+    "You find yourself in a dark room, the only light coming from a torch. It looks empty.", 
+    "You leave the dark room and head to the next deistanation.", 
+    "the dark room you have started your journey in.",
+    [items.skull],[]
+)
+path_room = room.Room(
+    "Path",
+    "An empty path, leading to another room",
+    "You continue on your jorney",
+    "a path.",
+    [],[]
+)
